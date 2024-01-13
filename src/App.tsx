252f4f544ad22ffa6f6ca7f4099e840e8576786f
Ruan-Amorim <ruanamorim0708@gmail.com>
2024-01-13 00:29:33 -0300

    Mensagem de commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,23 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import './App.css';
-import Hero from './Componentes/Players/Hero';
 import Masmorra from './Componentes/Mapas/Masmorra';
+import Hero from './Componentes/Players/Hero'
 import DemonLevel1 from './Componentes/Monsters/DemonLevel1';
 import DemonLevel2 from './Componentes/Monsters/DemonLevel2';
-import Controle from './Componentes/Botao';
 import Trap from './Componentes/Objetos/Trap';
 import Chest from './Componentes/Objetos/Chest';
 
 
 function App() {
+  
   return (
     <div className="App">
-     <Masmorra>
-     
-      <Hero />
+      <Masmorra>
       <DemonLevel1 />
       <DemonLevel2 />
-      <Trap />
-      <Chest />
-      
-      <Controle />
-     </Masmorra>
+      <Hero />
+      </Masmorra>
     </div>
   );
 }
-
 export default App;
--- src/Componentes/Players/Hero.tsx
+++ src/Componentes/Players/Hero.tsx
@@ -1,21 +1,81 @@
+import useEventListener from 'use-event-listener';
 import React from 'react';
+import { SIZE_1_BLOCK } from '../Sizes';
+
+const initialPosition = {
+  x: 10,
+  y: 10
+};
 
 const Hero = () => {
+  const [positionState, updatePositionState] = React.useState(initialPosition);
+  
+  useEventListener('keydown', (event) => {
+    if (event.key === 'w') {
+      updatePositionState({ x: positionState.x, y: positionState.y + 1})
+    }else if (event.key === 'a') {
+      updatePositionState({ x: positionState.x - 1, y: positionState.y})
+    }if (event.key === 's') {
+      updatePositionState({ x: positionState.x, y: positionState.y - 1})
+    }if (event.key === 'd') {
+      updatePositionState({ x: positionState.x + 1, y: positionState.y})
+    }
+  })
+  const faca = 1;
+  const up = () => {
+    if (faca == 1){
+      updatePositionState({ x: positionState.x, y: positionState.y + 1})
+    }else{}
+  };
+  const down = () => {
+    if (faca == 1){
+      updatePositionState({ x: positionState.x, y: positionState.y - 1})
+    }else{}
+  };
+  const right = () => {
+    if (faca == 1){
+      updatePositionState({ x: positionState.x + 1, y: positionState.y})
+    }else{}
+  };
+  const left = () => {
+    if (faca == 1){
+      updatePositionState({ x: positionState.x - 1, y: positionState.y})
+    }else{}
+  };
+  
   return (
+    <>
     <div style={{
        position: 'absolute',
-       width: 48,
-       height: 48,
+       width: SIZE_1_BLOCK,
+       height: SIZE_1_BLOCK,
        paddingTop: 13,
        
-       left: 48,
-       bottom: 48,
+       left: SIZE_1_BLOCK * positionState.x,
+       bottom: SIZE_1_BLOCK * positionState.y,
        
        backgroundImage: "url(./midia/HERO.png)",
        backgroundPosition: "left bottom",
        animation: "heroAnimation 0.9s steps(4) infinite",
        zIndex: 10,
-    }}/>
-  )
-}
+    }} />
+    
+    <div className="container">
+      <div className="controls">
+        <div className="move-buttons">
+          <button onClick={up} className="arrow-button up">&#9650;</button>
+          <button onClick={down} className="arrow-button down">&#9660;</button>
+          <button onClick={left} className="arrow-button left">&#9668;</button>
+          <button onClick={right} className="arrow-button right">&#9658;</button>
+        </div>
+      </div>
+      <div className="action-buttons">
+        <button className="attack-button">1</button>
+        <button className="attack-button">2</button>
+        <button className="attack-button">3</button>
+      </div>
+    </div>
+    </>
+  );
+};
 export default Hero;
